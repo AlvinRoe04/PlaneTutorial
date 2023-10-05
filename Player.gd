201bafd2a29ac36718a9ed53910f54479154fdb4
Alvin Roe <alvinroeiv@gmail.com>
2023-10-04 22:58:07 -0400

    End of Video 17

--- Player.gd
+++ Player.gd
@@ -2,3 +2,7 @@ extends CharacterBody2D
 
 func _process(delta):
 	position.y = position.y - 1
+	if Input.is_action_pressed('ui_right'):
+		position.x = position.x + 1
+	if Input.is_action_pressed("ui_left"):
+		position.x -= 1
